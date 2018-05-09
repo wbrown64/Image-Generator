68ca1fc94d14161baefda317b95d715789c38ab7
Jared <jrzymbaluk@gmail.com>
2018-05-09 15:45:34 -0600

    added methods and conclusion

---
+++ word.py
@@ -0,0 +1,13 @@
+import io
+from IPython.nbformat import current
+import glob
+nbfile = glob.glob('Project Report Example.ipynb')
+if len(nbfile) > 1:
+    print('More than one ipynb file. Using the first one.  nbfile=', nbfile)
+with io.open(nbfile[0], 'r', encoding='utf-8') as f:
+    nb = current.read(f, 'json')
+word_count = 0
+for cell in nb.worksheets[0].cells:
+    if cell.cell_type == "markdown":
+        word_count += len(cell['source'].replace('#', '').lstrip().split(' '))
+print('Word count for file', nbfile[0], 'is', word_count)
